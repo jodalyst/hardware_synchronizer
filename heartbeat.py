8d87c9dddaba457c5d261bc8800190d58db7deb4
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-05 21:34:20 -0400

    heartbeat sync add

--- heartbeat.py
+++ heartbeat.py
@@ -20,18 +20,22 @@ hal = 'https://eesjs1.net/testsite/hw_supervisor'
 
 while True:
     uname,password = [i.strip() for i in open(os.path.expanduser(lcl+'/.catsoop_login'))]
-    payload = {'user': uname, 'pword': password}
+    payload = {'user': uname, 'pword': password,'command':'status_query'}
     r = requests.get(hal, params=payload)
     print(r.text)
     k = json.loads(r.text)
-    print(k.keys())
-    if k['status'] =='code to run':
+    if k['state'] ==1:
+        payload = {'user': uname, 'pword': password,'command':'hw_command_retrieval'}
+        r = requests.post(hal, params=payload)
+        print(r)
+        k = json.loads(r.text)
+        data = k['hw_command']
         p = Popen(['python', '-'],stdin=PIPE,stdout=PIPE,stderr=PIPE)
         print('about to run')
         try:
             outs, errs = proc.communicate(input=data,timeout=PROCESS_TIMEOUT)
             print('got',outs)
-            payload['action'] = 'test_response'
+            payload['command'] = 'hw_response_provide'
             payload['response'] = outs
             r = requests.post(hal,params=payload)
             print('sent')
