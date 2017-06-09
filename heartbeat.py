22bbff9c785a89c7f73208f71866b1fee34009f6
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-09 13:26:24 -0400

    updating heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -8,6 +8,7 @@ import random
 import socket
 import string
 import multiprocessing
+import sys
 
 
 CHECKING_PERIOD = 5 #seconds
@@ -19,32 +20,50 @@ hal = 'https://eesjs1.net/testsite/hw_supervisor'
 
 
 while True:
+    print("Here")
+    while time.time()-timing <CHECKING_PERIOD:
+        print("waiting")
+        time.sleep(1)
+    timing = time.time()
     uname,password = [i.strip() for i in open(os.path.expanduser(lcl+'/.catsoop_login'))]
     payload = {'user': uname, 'pword': password,'command':'status_query'}
-    r = requests.get(hal, params=payload)
-    print(r.text)
-    k = json.loads(r.text)
-    if k['state'] ==1:
-        payload = {'user': uname, 'pword': password,'command':'hw_command_retrieval'}
-        r = requests.post(hal, params=payload)
-        print(r)
+    try:
+        print('requesting')
+        r = requests.get(hal, params=payload, timeout=1.0)
+        print('requested')
+        print("requesting at " + str(time.time()))
+        print(r.text)
         k = json.loads(r.text)
-        data = k['hw_command']
-        p = Popen(['python', '-'],stdin=PIPE,stdout=PIPE,stderr=PIPE)
-        print('about to run')
-        try:
-            outs, errs = proc.communicate(input=data,timeout=PROCESS_TIMEOUT)
-            print('got',outs)
-            payload['command'] = 'hw_response_provide'
-            payload['response'] = outs
-            r = requests.post(hal,params=payload)
-            print('sent')
-        except TimeoutExpired:
-            proc.kill()
-            payload['action'] = 'test_response'
-            payload['response'] = 'TIMEOUT FAILURE'
-            r = requests.post(hal,params=payload)
-            print('sent with timeout failure')
-    while time.time()-timing <CHECKING_PERIOD:
-        time.sleep(0.1)
-    timing = time.time()
+        if k['state'] in [1, 2]:
+            print("must run")
+            payload = {'user': uname, 'pword': password,'command':'hw_command_retrieval'}
+            r = requests.post(hal, data=payload, timeout=1.0)
+            print(r)
+            k = json.loads(r.text)
+            data = k['hw_command']
+            p = Popen(['python', '-'],stdin=PIPE,stdout=PIPE,stderr=PIPE)
+            print('about to run')
+            try:
+                outs, errs = p.communicate(input=data.encode(),timeout=PROCESS_TIMEOUT)
+                print('got',outs)
+                payload['command'] = 'hw_response_provide'
+                payload['hw_resp'] = outs
+                r = requests.post(hal,data=payload, timeout=1.0)
+                print('sent')
+            except:
+                proc.kill()
+                payload['command'] = 'hw_response_provide'
+                payload['hw_resp'] = 'TIMEOUT'
+                r = requests.post(hal,data=payload, timeout=1.0)
+                print('Exception')
+                print(sys.exc_info()[0])
+    except requests.exceptions.RequestException as e:
+        print("CONNECTION ISSUE AT " + str(time.time()))
+        print(e)
+        pass
+    except requests.exceptions.Timeout:
+        print("timeout")
+        pass
+    except requests.exceptions.TooManyRedirects:
+        print("crappy url")
+        pass
